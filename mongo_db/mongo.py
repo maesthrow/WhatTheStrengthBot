97578a74ff0253c5717a66ba65ac6bfb8bd40ec4
Dmitriy Kargin <dmitriy63kargin@gmail.com>
2024-07-06 15:36:34 +0400

    upd mongo prod config

--- mongo_db/mongo.py
+++ mongo_db/mongo.py
@@ -1,9 +1,16 @@
 import logging
+import os
 
 from pymongo import MongoClient
 
+mongo_username = os.getenv("MONGO_ROOT_USERNAME")
+mongo_password = os.getenv("MONGO_ROOT_PASSWORD")
+
+# amvera
+uri = f"mongodb://{mongo_username}:{mongo_password}@amvera-maesthrow-run-hk-mongo-db:27017"
+
 # local:
-uri = "mongodb://localhost:27017"
+# uri = "mongodb://localhost:27017"
 
 client = MongoClient(uri)
 
