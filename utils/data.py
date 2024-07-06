c4735e30f4b17702e2985fb947dd0daceba2b74c
Dmitriy Kargin <dmitriy63kargin@gmail.com>
2024-07-06 17:10:33 +0400

    fix remove publish_date

--- utils/data.py
+++ utils/data.py
@@ -3,7 +3,7 @@ from mongo_db.mongo_collection_videos import get_videos_collection, set_video_da
 
 async def remove_publish_dates():
     videos = await get_videos_collection()
-    async for video in videos.find():
-        video_data = video  # уже содержит данные, нет нужды делать дополнительный запрос
+    for video in videos.find():
+        video_data = video
         video_data['publish_date'] = None
         await set_video_data(video['_id'], video_data)
